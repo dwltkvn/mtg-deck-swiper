99cc8fc796ef33b83d40de1a5d05c5eab25bc412
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2021-01-07 20:21:57 +0100

    Remoe cache mechanism (not enought storage to store a deck)

--- src/components/card.js
+++ src/components/card.js
@@ -9,23 +9,13 @@ class Card extends React.Component {
     super(props)
 
     // this.handeEvent = this.handleEvent.bind(this);
-    this.imgLoaded = this.imgLoaded.bind(this)
     this.name = props.propCardName
     this.position = props.propCardPosition
-    if (typeof window !== `undefined`) {
-      this.img = new window.Image()
-    } else {
-      this.img = { onload: null, src: null, width: 0, height: 0 }
-    }
-    //this.img.crossOrigin = "Anonymous"
-    this.img.onload = () => {
-      this.imgLoaded()
-    }
+
     this.imgUrl =
       "https://c1.scryfall.com/file/scryfall-cards/large/front/f/5/f56861a7-b664-468f-bad7-838c02530827.jpg?1541002783"
     this.state = {
-      stateImg: this.props.propDisplayImg ? this.imgUrl : TmpCard,
-      stateLoadedFromCache: false
+      stateImg: this.props.propDisplayImg ? this.imgUrl : TmpCard
     }
   }
 
@@ -37,33 +27,13 @@ class Card extends React.Component {
     if (prevProps.propDisplayImg !== this.props.propDisplayImg) {
       console.log(`Display Img ${this.position}`)
 
-      let localImgData = localStorage.getItem(encodeURI(this.name))
-      if (localImgData) {
-        console.log("Loaded from cache")
-        this.setState({ stateImg: localImgData, stateLoadedFromCache: true })
-        return
-      }
-
-      this.setState({ stateLoadedFromCache: false })
       console.log(
-        `https://api.scryfall.com/cards/named?exact=${encodeURI(
-          this.name
-        )}&format=image&version=normal`
+        `https://api.scryfall.com/cards/named?exact=${encodeURI(this.name)}`
       )
       fetch(
-        `https://api.scryfall.com/cards/named?exact=${encodeURI(
-          this.name
-        )}&format=image&version=normal`
+        `https://api.scryfall.com/cards/named?exact=${encodeURI(this.name)}`
       )
-        .then(response => {
-          return response.blob()
-        })
-        .then(blob => {
-          //console.log(blob)
-          var objectURL = URL.createObjectURL(blob)
-          this.img.src = objectURL
-        })
-      /*.then(response => response.json())
+        .then(response => response.json())
         .then(data => {
           let stateImg = ""
 
@@ -72,36 +42,12 @@ class Card extends React.Component {
           else stateImg = data.image_uris.small
 
           //localStorage.setItem(encodeURI(this.name), stateImg)
-          //this.setState({ stateImg })
-          this.img.src = stateImg
-        })*/
+          this.setState({ stateImg })
+          //this.img.src = stateImg
+        })
     }
   }
 
-  imgLoaded() {
-    console.log("img loaded2")
-    console.log(this.img.height)
-    console.log(this.img.width)
-    console.log(this.img)
-
-    //localStorage.setItem(encodeURI(this.name), stateImg)
-    var canvas = document.createElement("canvas")
-    canvas.width = this.img.width
-    canvas.height = this.img.height
-
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext("2d")
-    ctx.drawImage(this.img, 0, 0)
-
-    // Get the data-URL formatted image
-    // Firefox supports PNG and JPEG. You could check img.src to
-    // guess the original format, but be aware the using "image/jpg"
-    // will re-encode the image.
-    var dataURL = canvas.toDataURL("image/jpeg")
-    //console.log(dataURL)
-    localStorage.setItem(encodeURI(this.name), dataURL)
-    this.setState({ stateImg: dataURL })
-  }
   render() {
     //const {classes} = this.props;
     //const {myState} = this.state;
@@ -126,9 +72,7 @@ class Card extends React.Component {
         backgroundSize="contain"
         //style={{ filter: `invert(${(this.position % 4) * 20}%)` }}
       >
-        {this.props.propTopCard
-          ? this.position + (this.state.stateLoadedFromCache ? "(C)" : "L")
-          : null}
+        {this.props.propTopCard ? this.position : null}
       </x.div>
     )
   }
