f7ffb068c8e3fe3d44185834911e257124d37973
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2021-01-12 00:03:51 +0100

    Update pwa display + short name

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,11 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `mtg-deck-swiper`,
-        short_name: `starter`,
+        short_name: `DeckSwiper`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
