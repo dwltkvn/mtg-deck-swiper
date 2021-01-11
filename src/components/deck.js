c1a2b966287a589a07a08a4acf4bc8a5924cfe3d
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2021-01-12 00:20:16 +0100

    add deck shuffler https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array

--- src/components/deck.js
+++ src/components/deck.js
@@ -105,6 +105,7 @@ class Deck extends React.Component {
   componentDidMount() {
     console.log("well mounted")
 
+    this.setState({ stateCardsIDs: this.shuffle(cardsIDs) })
     if (!("indexedDB" in window)) {
       console.log("This browser doesn't support IndexedDB")
       return
@@ -136,6 +137,26 @@ class Deck extends React.Component {
     if (this.database) {
       this.database.close()
     }
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    }
+
+    return array
   }
 
   render() {
